9fa1e58be6e0f9b604b3866dea26e0b12ccc07d2
bujosa <davidbujosa@gmail.com>
2023-10-26 19:23:38 -0400

    feat: connect succesufully to my database

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
@@ -16,13 +23,6 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var name string
-	var weight int64
-	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(name, weight)
+	// Perform your database operations here
+	fmt.Println("Successfully connected to the PostgreSQL database!")
 }
