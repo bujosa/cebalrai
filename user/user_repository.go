e58ad3de79dd4237ae16cbeee1e553bcb52cf8b4
bujosa <davidbujosa@gmail.com>
2023-10-28 04:01:43 -0400

    fix(user): update user repository query

--- user/user_repository.go
+++ user/user_repository.go
@@ -1,1 +1,20 @@
 package user
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type UserRepository struct {
+    db *pgx.Conn // Database connection
+}
+
+func (ur *UserRepository) GetUserByID(ctx context.Context, userID int) (*User, error) {
+    var user User
+	err := ur.db.QueryRow(ctx, "SELECT id, name, email FROM users WHERE id = $1", userID).Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
