8bb4ccd480a6611b9b8abd8b1da6cd33b36087f7
bujosa <davidbujosa@gmail.com>
2023-10-28 04:01:59 -0400

    feat: add user service logic for get user by id

--- user/user_service.go
+++ user/user_service.go
@@ -1,1 +1,14 @@
 package user
+
+import (
+	"context"
+)
+
+type UserService struct {
+    userRepository *UserRepository
+}
+
+func (us *UserService) GetUserByID(ctx context.Context, userID int) (*User, error) {
+    // In this case, we can add some logic before calling the repository
+    return us.userRepository.GetUserByID(ctx, userID)
+}
